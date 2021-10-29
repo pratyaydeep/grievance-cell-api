23d6baf8bc4f5aa071271c0fde9d4d15522bd81b
pratyaydeep <pratyaydeepghanta@gmail.com>
2021-10-29 23:56:49 +0530

    Initial Commit

---
+++ src/main/java/com/examly/springapp/controllers/DemoController.java
@@ -0,0 +1,22 @@
+//package com.examly.springapp.controllers;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class DemoController {
+//	@GetMapping("/admin")
+//	public String admin() {
+//		return "Hello Admin";
+//	}
+//	
+//	@GetMapping("/employee")
+//	public String employee() {
+//		return "Hello Employee";
+//	}
+//	
+//	@GetMapping("/user")
+//	public String user() {
+//		return "Hello User";
+//	}
+//}
